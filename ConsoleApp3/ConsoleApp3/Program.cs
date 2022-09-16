68610b4c115c9f2f1b635387f2a290c4f1e15472
Dipteshpatra <113527751+Dipteshpatra@users.noreply.github.com>
2022-09-16 19:02:11 +0530

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = new int[10];
+            Console.WriteLine("enter the array elements");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            Array.Reverse(arr);
+          
+            Console.WriteLine("secod largest number in the array:"+arr[1]);
+        }
+    }
+}
